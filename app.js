d4c155e17ef7015459595f1109a85a175ed32b05
Alex Strickland <alexstrickland@Alexs-MacBook-Pro.local>
2022-10-10 19:58:12 -0500

    read only, not full read

--- app.js
+++ app.js
@@ -3,53 +3,48 @@ const path = require('node:path')
 const bodyParser = require('body-parser')
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { resourceLimits } = require('node:worker_threads');
 const uri = "mongodb+srv://dev-papa:yes123@dev-lab-papa.6opbmby.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.use(bodyParser.json())
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000
 
-console
+  async function main(){
+    try {
+        // Connect to the MongoDB cluster
+         client.connect(err => {
+            const collection = client.db("sample_guides").collection("planets");
+            console.log('connected');
+            console.log('console log closed');
+          });;
+        // Make the appropriate DB calls
+        databasesList = await client.db().admin().listDatabases();
+        console.log("Databases:");
+        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+        posts = await client.db("sample_guides").collection("planets").findOne();
+        // indxs =  await posts.indexes();
+        // console.log(indxs);
+        console.log(posts); 
+        return posts; 
+        // return posts.findOne();
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+// main().catch(console.error);
+app.get('/', async function (req, res) {
+    const result = await main().catch(console.error);
+    console.log("results: ", result); 
+    res.send(`results:  ${result.name}`); 
+    });
 
-app.get('/', function (req, res) {
-    // res.sendFile(path.join(__dirname, "index.html" )); 
-    // res.send('Hello ' + userName + ' from Node/Express/Heroku');
-    // res.send(`Hello ${userName} from Node/Express/Heroku with Backticks!`)
-    res.send('hello express from inside of client connect')
-    // res.render('index', 
-    //   { 
-    //     userName: userName
-    //   }
-    //   );
-// })
-// app.post("/saveToNode", (req, res) => {
-//   console.log(req.body);
-//   console.group(req.body.userName);
-//   res.render('index', { userName: req.body.userName });
-
-// })
-
-// app.listen(process.env.PORT || 3000,
-//   () => console.log("Server is running on port:..." `${process.env.PORT}`));
 
 
-console.log('in app.js');
-// const result = collection.find();
-// console.log(result);
-client.connect(err => {
-  const collection = client.db("dev-lab-papa").collection("sample_restaurants");
-  console.log('connected');
-  res.send('hello express from inside of client connect');
-  const result = collection.find();
-  console.log(result);
-  res.send(result);
-  console.log('console log closed');
-});
-});
-
-// Below code should add to my mongo db
-// const collection = client.db("sample_mflix").collection("movies");
-// const favorite_movies = await collection.find({genres : ["Drama", "Fantasy"]});
-
-app.listen(process.env.PORT || 3000,
-  () => console.log(`server is running on port: ${process.env.PORT}` ));
+    
+  
+app.listen(PORT, console.log(`server is running on port: ${PORT}` ));
